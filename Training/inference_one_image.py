0e9c40458430850faf7f454b5eb52df567522915
wtomin <dengdidanwtomin@gmail.com>
2021-07-10 20:29:16 +0800

    inference image

--- Training/inference_one_image.py
+++ Training/inference_one_image.py
@@ -16,7 +16,13 @@ import pickle
 from utils.transforms import test_transforms
 from utils.validation import sigmoid, softmax
 from scipy.stats import entropy
-
+import matplotlib
+import matplotlib.pyplot as plt
+font = {
+        'size'   : 35}
+matplotlib.rc('font', **font)
+plt.rcParams.update({'figure.autolayout': True})
+plt.rcParams["font.family"] = "serif"
 #################RuntimeError: received 0 items of ancdata ###########################
 import torch
 torch.multiprocessing.set_sharing_strategy("file_system")
@@ -79,37 +85,72 @@ class Tester(object):
         self._model = model
 
         estimates, uncertainties = self._test()
-        self.print_resutls(estimates, uncertainties)
-    def print_resutls(self, estimates, uncertainties):
+        annt_img, results = self.print_results(estimates, uncertainties)
+        self.plot_results(annt_img, results)
+
+    def plot_results(self, annt_img, results):
+        fig, axes = plt.subplots(1, 2, figsize=(18, 12))
+        axes[0].imshow(np.array(annt_img))
+        axes[0].set_axis_off()
+
+        group_names = ['AU1', 'AU2', 'AU4', 'AU6', 'AU7', 'AU10', 'AU12', 'AU15','AU23','AU24', 'AU25','AU26']
+        group_data = [ results[t][1] for t in group_names] 
+        group_names = ['{}: {}'.format(t, results[t][0]) for t in group_names] 
+
+        group_names += [results['EXPR'][0]]
+        group_data += [results['EXPR'][1]]
+        group_names += ['Valence: {:.1f}'.format(results['Valence'][0]), 'Arousal: {:.1f}'.format(results['Arousal'][0])]
+        group_data += [results['Valence'][1], results['Arousal'][1]]
+        group_names.reverse()
+        group_data.reverse()
+        axes[1].barh(group_names+group_names, group_data+group_data,  height=0.2)
+        axes[1].set_xlim([0, 1])
+        axes[1].get_xaxis().set_ticks([0, 1])
+        axes[1].set_xlabel("Emotion Uncertainty")
+
+        #plt.figtext(0.7,0.03,"Emotion Uncertainty", va="center", ha="center")
+        plt.figtext(0.2,0.1,"Landmarks", va="center", ha="center")
+        plt.tight_layout()
+        plt.savefig('inference_img.pdf')
+        plt.show()
+
+    def print_results(self, estimates, uncertainties):
+        results = {}
         for task in estimates:
             if task == 'FA':
                 landmark = estimates['FA']
                 landmark = landmark.reshape(-1, 2)
-                image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)  # PIL转cv2
+                image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR) 
                 for x, y in landmark:
                     cv2.circle(image, (int(x*out_size), int(y*out_size)), 2, (0,255,0), -1)
-                Image.fromarray(image[:, :, ::-1]).show()
+                annt_img = Image.fromarray(image[:, :, ::-1])
+
             elif task == 'AU':
                 categories = PRESET_VARS.Aff_wild2.categories['AU']
                 output = ' '.join(["{}: {} ({:.1f}%)".format(categories[i], estimates[task][i], uncertainties[task][i]*100) for i in range(len(estimates[task]))])
                 print(output)
+                for i in range(len(categories)):
+                    results[categories[i]] = [estimates[task][i], uncertainties[task][i]]
             elif task == 'EXPR':
                 categories = PRESET_VARS.Aff_wild2.categories['EXPR']
                 o = estimates[task]
                 p = uncertainties[task]
                 output = "{}: {:.1f}%".format(categories[o], p*100)
                 print(output)
+                results['EXPR'] = [categories[o], p]
             elif task == 'Valence' or task == 'Arousal':
                 N = 20
                 o = estimates[task]
                 p = uncertainties[task]
                 print("{}: {:.2f}".format(task, o))
-                edges = np.linspace(-1, 1, num= N+1)
-                dig = np.digitize(o, edges) - 1
-                if dig==N:
-                    dig  = N-1
-                start, end = edges[dig], edges[dig+1]
-                print("{:.2f} ~ {:.2f}: {:.2f}%".format(start, end, p))
+                # edges = np.linspace(-1, 1, num= N+1)
+                # dig = np.digitize(o, edges) - 1
+                # if dig==N:
+                #     dig  = N-1
+                # start, end = edges[dig], edges[dig+1]
+                print("uncertainty: {:.2f}%".format( p*100))
+                results[task] = [o, p]
+        return annt_img, results
     def _test(self):
         example_task = 'AU'
         hiddens = dict([t, None] for t in args.tasks)
@@ -128,17 +169,14 @@ class Tester(object):
                 estimates['AU'] = o.numpy()
                 uncertainties['AU'] = []
                 for i_o, o in enumerate( estimates['AU']):
-                    if o ==0:
-                        uncertainties['AU'].append(1- probas.numpy()[i_o])
-                    else:
-                        uncertainties['AU'].append(probas.numpy()[i_o])
+                    uncertainties['AU'].append(entropy([probas.numpy()[i_o], 1- probas.numpy()[i_o]], base=2)/np.log2(2))
                 uncertainties['AU'] = np.array(uncertainties['AU'])
 
             elif task == 'EXPR':
                 probas = F.softmax(output['EXPR'].squeeze().cpu(), dim=-1)
                 o = probas.argmax(-1).type(torch.LongTensor)
                 estimates['EXPR'] = o.numpy()
-                uncertainties['EXPR'] = probas[o]
+                uncertainties['EXPR'] = entropy(probas, base=2)/np.log2(7)
             elif task == 'VA':
                 N = 20
                 pv = F.softmax(output['VA'][:,:, :N].cpu(), dim=-1).squeeze().numpy()
@@ -148,16 +186,16 @@ class Tester(object):
                 a = (bins * pa).sum(-1)
                 estimates['Valence'] = v
                 estimates['Arousal'] = a
-                # discretize the predictions
-                edges = np.linspace(-1, 1, num= N+1)
-                v_dig = np.digitize(v, edges) - 1
-                if v_dig==N:
-                    v_dig = N -1
-                a_dig = np.digitize(a, edges) - 1
-                if a_dig ==N:
-                    a_dig = N- 1
-                uncertainties['Valence'] = pv[v_dig]
-                uncertainties['Arousal'] = pa[a_dig]
+                # # discretize the predictions
+                # edges = np.linspace(-1, 1, num= N+1)
+                # v_dig = np.digitize(v, edges) - 1
+                # if v_dig==N:
+                #     v_dig = N -1
+                # a_dig = np.digitize(a, edges) - 1
+                # if a_dig ==N:
+                #     a_dig = N- 1
+                uncertainties['Valence'] = entropy(pv, base=2)/np.log2(N)
+                uncertainties['Arousal'] = entropy(pa, base=2)/np.log2(N)
             elif task == 'FA':
                 estimates['FA'] = output['FA'].cpu().numpy()
                 uncertainties['FA'] = None
