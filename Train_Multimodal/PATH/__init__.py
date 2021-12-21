dab573a7727db3cd4e882429773221562e6a4991
wtomin <dengdidanwtomin@gmail.com>
2021-12-21 20:08:38 +0800

    test_set.pkl

--- Train_Multimodal/PATH/__init__.py
+++ Train_Multimodal/PATH/__init__.py
@@ -4,7 +4,7 @@ Dataset_Info = collections.namedtuple("Dataset_Info", 'data_file test_data_file 
 class PATH(object):
     def __init__(self):
         self.Aff_wild2 = Dataset_Info(data_file = '../Training/annotations/annotations.pkl',
-         test_data_file = '/media/Samsung/Aff-wild2-Challenge/test_set/test_set.pkl',
+         test_data_file = '../test_set/test_set.pkl',
             categories = {'AU': ['AU1','AU2','AU4','AU6','AU7','AU10','AU12','AU15','AU23','AU24','AU25','AU26'],
                             'EXPR':['Neutral','Anger','Disgust','Fear','Happiness','Sadness','Surprise'],
                             'VA':['valence', 'arousal']})
