44349970e6383a22c098edceb35dc7a7b82faa13
wtomin <dengdidanwtomin@gmail.com>
2022-02-13 15:44:58 +0800

    path

---
+++ Train_Image/PATH/__init__.py
@@ -0,0 +1,12 @@
+import collections
+
+Dataset_Info = collections.namedtuple("Dataset_Info", 'data_file test_data_file categories')
+class PATH(object):
+    def __init__(self):
+        self.Aff_wild2 = Dataset_Info(data_file = 'annotations/annotations.pkl',
+         test_data_file = '/media/Samsung/Aff-wild2-Challenge/test_set/test_set.pkl',
+            categories = {'AU': ['AU1','AU2','AU4','AU6','AU7','AU10','AU12','AU15','AU23','AU24','AU25','AU26'],
+                            'EXPR':['Neutral','Anger','Disgust','Fear','Happiness','Sadness','Surprise'],
+                            'VA':['valence', 'arousal']})
+        self.face_dir = '/media/Samsung/Aff-wild2-Challenge/cropped_aligned'
+
